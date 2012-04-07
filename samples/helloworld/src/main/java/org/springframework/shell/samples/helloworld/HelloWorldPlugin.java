ce64cfad34fb9327010c49fba81e60eb600d7de2
Jarred Li <jiali@vmware.com>
2012-04-07 19:14:16 +0800

    change folder structure

--- samples/helloworld/src/main/java/org/springframework/shell/samples/helloworld/HelloWorldPlugin.java
+++ samples/helloworld/src/main/java/org/springframework/shell/samples/helloworld/HelloWorldPlugin.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"org.springframework.shell.commands", "org.springframework.shell.converters"})
+@ComponentScan({"org.springframework.shell.commands", 
+				"org.springframework.shell.converters",
+				"org.springframework.shell.samples.helloworld.commands"})
 public class HelloWorldPlugin {
 	
 
