0ae6edba379891c45af4935fb056ef715485b827
Costin Leau <cleau@vmware.com>
2012-07-12 20:12:20 +0300

    add basic pre/post processing aka poor man's AOP
    SHL-40

--- src/main/java/org/springframework/shell/SimpleExecutionStrategy.java
+++ src/main/java/org/springframework/shell/SimpleExecutionStrategy.java
@@ -1,20 +1,45 @@
 package org.springframework.shell;
 
+import org.springframework.roo.shell.CommandMarker;
 import org.springframework.roo.shell.ExecutionStrategy;
 import org.springframework.roo.shell.ParseResult;
 import org.springframework.roo.support.util.Assert;
 import org.springframework.roo.support.util.ReflectionUtils;
 
+/**
+ * Simple execution strategy for invoking a target method.
+ * Supports pre/post processing to allow {@link CommandMarker}s for aop-like behavior (
+ * typically used for controlling stateful objects). 
+ * 
+ * @author Mark Pollack
+ * @author Costin Leau
+ */
 public class SimpleExecutionStrategy implements ExecutionStrategy {
 
 	private final Class<?> mutex = SimpleExecutionStrategy.class;
-	
+
 	public Object execute(ParseResult parseResult) throws RuntimeException {
 		Assert.notNull(parseResult, "Parse result required");
 		synchronized (mutex) {
 			Assert.isTrue(isReadyForCommands(), "ProcessManagerHostedExecutionStrategy not yet ready for commands");
-			return ReflectionUtils.invokeMethod(parseResult.getMethod(), parseResult.getInstance(), parseResult.getArguments());
+			Object target = parseResult.getInstance();
+			if (target instanceof ExecutionProcessor) {
+				ExecutionProcessor processor = ((ExecutionProcessor) target);
+				parseResult = processor.beforeInvocation(parseResult);
+				try {
+					return invoke(parseResult);
+				} finally {
+					processor.afterInvocation(parseResult);
+				}
+			}
+			else {
+				return invoke(parseResult);
+			}
 		}
+	}
+
+	private Object invoke(ParseResult parseResult) {
+		return ReflectionUtils.invokeMethod(parseResult.getMethod(), parseResult.getInstance(), parseResult.getArguments());
 	}
 
 	public boolean isReadyForCommands() {
