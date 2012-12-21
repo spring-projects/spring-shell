928c7aad94acf11582ba1ecb52df2aa65dc4ffc8
Jon Brisbin <jon@jbrisbin.com>
2012-12-21 09:21:31 -0600

    Revert "Fix Bootstrap test by not staring the shell and hence other threads that block the completion of tests run via gradle"
    This reverts commit f317d21f04d7df7033c475d6b3c655b04e9203da.

--- src/test/java/org/springframework/shell/BootstrapTest.java
+++ src/test/java/org/springframework/shell/BootstrapTest.java
@@ -1,5 +1,7 @@
 package org.springframework.shell;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -7,18 +9,24 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.springframework.shell.core.JLineShellComponent;
+import org.springframework.shell.event.ShellStatus;
+import org.springframework.shell.event.ShellStatus.Status;
 import org.springframework.shell.support.logging.HandlerUtils;
 
 public class BootstrapTest {
 
 	@Test
-	public void test() throws IOException {
+	public void test() throws IOException, InterruptedException {
 		try {
 			Bootstrap bootstrap = new Bootstrap(null);
 			JLineShellComponent shell = bootstrap.getJLineShellComponent();
-			System.out.println("about to start shell...");
+			shell.start();
+			while (shell.getShellStatus().getStatus() != ShellStatus.Status.USER_INPUT) {
+				Thread.sleep(100);
+			}
 			Assert.assertEquals("Number of CommandMarkers is incorrect", 4, shell.getSimpleParser().getCommandMarkers().size());
-			Assert.assertEquals("Number of Converters is incorrect", 16, shell.getSimpleParser().getConverters().size());			
+			Assert.assertEquals("Number of Converters is incorrect", 16, shell.getSimpleParser().getConverters().size());
+			shell.stop();
 		} catch (RuntimeException t) {
 			throw t;
 		} finally {
