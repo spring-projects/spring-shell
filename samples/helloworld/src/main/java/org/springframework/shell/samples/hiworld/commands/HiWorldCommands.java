684542af96fbcf6124e8cfe8b871770029ee6960
Robin Howlett <rhowlett@sportslabs.com>
2014-12-16 16:14:48 -0700

    SHL-106: Java Configuration support
    - Updated Bootstrap to accept basePackages String varargs and to include them in ClassPathBeanDefinitionScanner scan
    - Updated sample HelloWorld project to demonstrate mixing XML and Java Configuration
    - Updated docbook-reference-plugin dependency group ID and version

---
+++ samples/helloworld/src/main/java/org/springframework/shell/samples/hiworld/commands/HiWorldCommands.java
@@ -0,0 +1,21 @@
+package org.springframework.shell.samples.hiworld.commands;
+
+import org.springframework.shell.core.CommandMarker;
+import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.stereotype.Component;
+
+/**
+ * Separate CommandMarker implementation to demonstrate mixing
+ * XML and Java-based Configurations 
+ * 
+ * @author Robin Howlett (@robinhowlett)
+ *
+ */
+@Component
+public class HiWorldCommands implements CommandMarker {
+	
+	@CliCommand(value = "hi", help = "Print a Hi World! message")
+	public String hi() {
+		return "Hi World!";
+	}
+}
