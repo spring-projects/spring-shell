01be0ec7fcc3e2cb9f7aa51d6a64bfb0e4830d96
Eric Bottard <eric.bottard+github@gmail.com>
2018-01-15 11:00:09 +0100

    Decouple ApplicationRunners

--- spring-shell-core/src/main/java/org/springframework/shell/jline/InteractiveShellApplicationRunner.java
+++ spring-shell-core/src/main/java/org/springframework/shell/jline/InteractiveShellApplicationRunner.java
@@ -16,11 +16,7 @@
 
 package org.springframework.shell.jline;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 import org.jline.reader.LineReader;
 import org.jline.reader.Parser;
@@ -30,27 +26,37 @@ import org.jline.utils.AttributedString;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.shell.ExitRequest;
 import org.springframework.shell.Input;
 import org.springframework.shell.InputProvider;
 import org.springframework.shell.Shell;
 
 /**
- * Default Boot runner that bootstraps the shell application.
+ * Default Boot runner that bootstraps the shell application in interactive mode.
  *
  * <p>
- *     Default implementation has default priority and looks for application arguments that start with an {@literal @},
- *     assuming they are paths to script files. Executes them and quits if they are present, starts the shell interactively
- *     otherwise.
+ *     Runs the REPL of the shell unless the {@literal spring.shell.interactive} property has been set to {@literal false}.
  * </p>
  *
  * @author Eric Bottard
  */
-//tag::documentation[]
-@Order(DefaultShellApplicationRunner.PRECEDENCE)
-public class DefaultShellApplicationRunner implements ApplicationRunner {
-//end::documentation[]
+@Order(InteractiveShellApplicationRunner.PRECEDENCE)
+public class InteractiveShellApplicationRunner implements ApplicationRunner {
+
+	/**
+	 * The precedence at which this runner is set. Highger precedence runners may effectively disable this one by setting
+	 * the {@link #SPRING_SHELL_INTERACTIVE_ENABLED} property to {@literal false}.
+	 */
 	public static final int PRECEDENCE = 0;
+
+	public static final String SPRING_SHELL_INTERACTIVE = "spring.shell.interactive";
+	public static final String ENABLED = "enabled";
+
+	/** The name of the property that controls whether this runner effectively does something. */
+	public static final String SPRING_SHELL_INTERACTIVE_ENABLED = SPRING_SHELL_INTERACTIVE + "." + ENABLED;
 
 	private final LineReader lineReader;
 
@@ -60,33 +66,33 @@ public class DefaultShellApplicationRunner implements ApplicationRunner {
 
 	private final Shell shell;
 
-	public DefaultShellApplicationRunner(LineReader lineReader, PromptProvider promptProvider, Parser parser, Shell shell) {
+	private final Environment environment;
+
+	public InteractiveShellApplicationRunner(LineReader lineReader, PromptProvider promptProvider, Parser parser, Shell shell, Environment environment) {
 		this.lineReader = lineReader;
 		this.promptProvider = promptProvider;
 		this.parser = parser;
 		this.shell = shell;
+		this.environment = environment;
 	}
 
-	//tag::documentation[]
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		List<File> scriptsToRun = args.getNonOptionArgs().stream()
-				.filter(s -> s.startsWith("@"))
-				.map(s -> new File(s.substring(1)))
-				.collect(Collectors.toList());
-
-		if (scriptsToRun.isEmpty()) {
+		boolean interactive = environment.getProperty(SPRING_SHELL_INTERACTIVE_ENABLED,boolean.class,  true);
+		if (interactive) {
 			InputProvider inputProvider = new JLineInputProvider(lineReader, promptProvider);
 			shell.run(inputProvider);
-		} else {
-			for (File file : scriptsToRun) {
-				try (Reader reader = new FileReader(file); FileInputProvider inputProvider = new FileInputProvider(reader, parser)) {
-					shell.run(inputProvider);
-				}
-			}
 		}
 	}
-	//end::documentation[]
+
+	/**
+	 * Helper method to dynamically disable this runner.
+	 */
+	public static void disable(ConfigurableEnvironment environment) {
+		environment.getPropertySources().addFirst(new MapPropertySource("interactive.override",
+				Collections.singletonMap(SPRING_SHELL_INTERACTIVE_ENABLED, "false")));
+	}
+
 	public static class JLineInputProvider implements InputProvider {
 
 		private final LineReader lineReader;
--- spring-shell-samples/src/main/java/org/springframework/shell/samples/ExampleApplicationRunnerConfiguration.java
+++ spring-shell-samples/src/main/java/org/springframework/shell/samples/ExampleApplicationRunnerConfiguration.java
@@ -17,22 +17,21 @@
 package org.springframework.shell.samples;
 
 import java.util.List;
-
-import org.jline.reader.LineReader;
-import org.jline.reader.Parser;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ExitCodeExceptionMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.shell.ExitRequest;
 import org.springframework.shell.Input;
 import org.springframework.shell.InputProvider;
 import org.springframework.shell.Shell;
-import org.springframework.shell.jline.DefaultShellApplicationRunner;
-import org.springframework.shell.jline.PromptProvider;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.util.StringUtils;
 
 @Configuration
@@ -42,13 +41,19 @@ public class ExampleApplicationRunnerConfiguration {
 	private Shell shell;
 
 	@Bean
-	public ApplicationRunner exampleApplicationRunner() {
-		return new ExampleApplicationRunner(shell);
+	public ApplicationRunner exampleApplicationRunner(ConfigurableEnvironment environment) {
+		return new ExampleApplicationRunner(shell, environment);
 	}
 
 	@Bean
-	public ApplicationRunner defaultApplicationRunner(LineReader lineReader, PromptProvider promptProvider, Parser parser) {
-		return new DefaultShellApplicationRunner(lineReader, promptProvider, parser, shell);
+	public ExitCodeExceptionMapper exitCodeExceptionMapper() {
+		return exception -> {
+			Throwable e = exception;
+			while (e != null && !(e instanceof ExitRequest)) {
+				e = e.getCause();
+			}
+			return e == null ? 1 : ((ExitRequest) e).status();
+		};
 	}
 }
 
@@ -57,20 +62,26 @@ public class ExampleApplicationRunnerConfiguration {
  * this particular example, any program (process) arguments are assumed to be shell
  * commands that need to be executed (and the shell then quits).
  */
-@Order(-2)
+@Order(InteractiveShellApplicationRunner.PRECEDENCE - 2)
 class ExampleApplicationRunner implements ApplicationRunner {
 
 	private Shell shell;
 
-	public ExampleApplicationRunner(Shell shell) {
+	private final ConfigurableEnvironment environment;
+
+	public ExampleApplicationRunner(Shell shell, ConfigurableEnvironment environment) {
 		this.shell = shell;
+		this.environment = environment;
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		if (!args.getNonOptionArgs().isEmpty()) {
-			shell.run(new StringInputProvider(args.getNonOptionArgs()));
-			throw new ExitRequest(0);
+		List<String> commandsToRun = args.getNonOptionArgs().stream()
+				.filter(w -> !w.startsWith("@"))
+				.collect(Collectors.toList());
+		if (!commandsToRun.isEmpty()) {
+			shell.run(new StringInputProvider(commandsToRun));
+			InteractiveShellApplicationRunner.disable(environment);
 		}
 	}
 }
@@ -100,7 +111,8 @@ class StringInputProvider implements InputProvider {
 					return StringUtils.collectionToDelimitedString(words, " ");
 				}
 			};
-		} else {
+		}
+		else {
 			return null;
 		}
 	}
