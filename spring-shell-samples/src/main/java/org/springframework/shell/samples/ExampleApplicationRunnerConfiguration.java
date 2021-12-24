54702460d1bb9e17c8cfeba287b69d9862938931
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-12-24 13:25:07 +0000

    Make sample to work with spring-native
    - While project uses jdk8, you can do native build with jdk11
      `./mvnw clean package -Pnative`
    - You can then run these
      spring-shell-samples/target/spring-shell-samples
      java -jar spring-shell-samples/target/spring-shell-samples-3.0.0-SNAPSHOT-exec.jar
    - Remove logback.xml in favour of spring config to workaround for
      https://github.com/spring-projects-experimental/spring-native/issues/625
    - For now remove jcommander command sample to get support for it #340
    - Relates #323

--- spring-shell-samples/src/main/java/org/springframework/shell/samples/ExampleApplicationRunnerConfiguration.java
+++ spring-shell-samples/src/main/java/org/springframework/shell/samples/ExampleApplicationRunnerConfiguration.java
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
- *      
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ import org.springframework.shell.Shell;
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.util.StringUtils;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class ExampleApplicationRunnerConfiguration {
 
 	@Autowired
