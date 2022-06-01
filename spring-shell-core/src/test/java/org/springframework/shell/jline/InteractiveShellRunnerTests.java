b6aeee9a8684ff58812bd8dad87ab66508e132a5
Yubi Lee <eubnara@gmail.com>
2022-06-02 01:33:13 +0900

    fix hang on testing

--- spring-shell-core/src/test/java/org/springframework/shell/jline/InteractiveShellRunnerTests.java
+++ spring-shell-core/src/test/java/org/springframework/shell/jline/InteractiveShellRunnerTests.java
@@ -2,8 +2,8 @@ package org.springframework.shell.jline;
 
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
+import org.jline.terminal.Attributes;
+import org.jline.terminal.impl.ExternalTerminal;
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
 import org.junit.jupiter.api.Test;
@@ -11,44 +11,122 @@ import org.springframework.shell.ExitRequest;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
 
 public class InteractiveShellRunnerTests {
 
+    private PipedOutputStream outIn;
+    private InteractiveShellRunner.JLineInputProvider jLineInputProvider;
 
-    public PromptProvider dummyPromptProvider() {
+
+    private PromptProvider dummyPromptProvider() {
         return () -> new AttributedString("dummy-shell:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
     }
 
-    @Test
-    public void testExitShortcuts() throws Exception {
-
+    private void initForShortcutKeyTest() throws Exception {
         PipedInputStream in = new PipedInputStream();
-        final PipedOutputStream outIn = new PipedOutputStream(in);
-        Terminal terminal = TerminalBuilder.builder()
-                .streams(in, System.out)
-                .nativeSignals(true)
-                .build();
+        outIn = new PipedOutputStream(in);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ExternalTerminal terminal = new ExternalTerminal("foo", "ansi", in, out, StandardCharsets.UTF_8);
+        Attributes attributes = terminal.getAttributes();
+        attributes.setLocalFlag(Attributes.LocalFlag.ISIG, true);
+        attributes.setControlChar(Attributes.ControlChar.VINTR, 3);
+        terminal.setAttributes(attributes);
         LineReaderBuilder builder =
                 LineReaderBuilder.builder()
                         .terminal(terminal);
 
-        LineReader reader = builder.build();
-        InteractiveShellRunner.JLineInputProvider jLineInputProvider = new InteractiveShellRunner.JLineInputProvider(reader, dummyPromptProvider());
+        LineReader lineReader = builder.build();
+        jLineInputProvider = new InteractiveShellRunner.JLineInputProvider(lineReader, dummyPromptProvider());
+    }
 
-        // clear with ctrl + c
-        outIn.write('a');
-        outIn.write(3); // ctrl + c
+    @Test
+    public void testClearWithCtrlC() throws Exception {
 
-        assertThatNoException().isThrownBy(jLineInputProvider::readInput);
+        initForShortcutKeyTest();
 
-        // exit with ctrl + c
-        outIn.write(3);
-        assertThatThrownBy(jLineInputProvider::readInput).isInstanceOf(ExitRequest.class);
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(1);
+        Thread writeThread = new Thread(() -> {
+            try {
+                startLatch.await();
+                outIn.write('a');
+                outIn.write(3);
+                endLatch.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Thread readThread = new Thread(() -> {
+            assertThatNoException().isThrownBy(() -> assertThat(jLineInputProvider.readInput().rawText()).isEqualTo(""));
+            endLatch.countDown();
+        });
+        readThread.start();
+        startLatch.countDown();
+        writeThread.start();
 
-        // exit with ctrl + d
-        outIn.write(4); // ctrl + d
-        assertThatThrownBy(jLineInputProvider::readInput).isInstanceOf(ExitRequest.class);
+        readThread.join();
+        writeThread.join();
+    }
+
+
+    @Test
+    public void testExitWithCtrlC() throws Exception {
+
+        initForShortcutKeyTest();
+
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(1);
+        Thread writeThread = new Thread(() -> {
+            try {
+                startLatch.await();
+                outIn.write(3);
+                endLatch.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Thread readThread = new Thread(() -> {
+            assertThatThrownBy(jLineInputProvider::readInput).isInstanceOf(ExitRequest.class);
+            endLatch.countDown();
+        });
+        readThread.start();
+        startLatch.countDown();
+        writeThread.start();
+
+        readThread.join();
+        writeThread.join();
+    }
+
+    @Test
+    public void testExitWithCtrlD() throws Exception {
+
+        initForShortcutKeyTest();
+
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(1);
+        Thread writeThread = new Thread(() -> {
+            try {
+                startLatch.await();
+                outIn.write(4);
+                endLatch.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Thread readThread = new Thread(() -> {
+            assertThatThrownBy(jLineInputProvider::readInput).isInstanceOf(ExitRequest.class);
+            endLatch.countDown();
+        });
+        readThread.start();
+        startLatch.countDown();
+        writeThread.start();
+
+        readThread.join();
+        writeThread.join();
     }
 }
