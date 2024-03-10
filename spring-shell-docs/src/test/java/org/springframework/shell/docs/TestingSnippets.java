d252482bf4ea4fd277e283d5d8c36163aeeccca5
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-03-10 15:44:21 +0000

    Fix docs sample
    - Backport #1012
    - Fixes #1032

--- spring-shell-docs/src/test/java/org/springframework/shell/docs/TestingSnippets.java
+++ spring-shell-docs/src/test/java/org/springframework/shell/docs/TestingSnippets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2022-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,7 +71,7 @@ class TestingSnippets {
 		@Test
 		void test() {
 			NonInteractiveShellSession session = client
-				.nonInterative("help", "help")
+				.nonInterative("help")
 				.run();
 
 			await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
