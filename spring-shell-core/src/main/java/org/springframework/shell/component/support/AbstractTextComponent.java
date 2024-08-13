1dbb35a8a784f627dcddfd41f1b8df478be9f8a6
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-08-13 14:26:32 +0100

    StringInput better unicode handling
    - Add unicode handling in flow components in
      AbstractTextComponent which allows StringInput
      to process unicode given from jline.
    - This add basic non-english character support
      for input.
    - Backport #1115
    - Fixes #1116

--- spring-shell-core/src/main/java/org/springframework/shell/component/support/AbstractTextComponent.java
+++ spring-shell-core/src/main/java/org/springframework/shell/component/support/AbstractTextComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2022-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,7 @@ public abstract class AbstractTextComponent<T, C extends TextComponentContext<T,
 		for (char i = 32; i < KeyMap.KEYMAP_LENGTH - 1; i++) {
 			keyMap.bind(OPERATION_CHAR, Character.toString(i));
 		}
+		keyMap.setUnicode(OPERATION_UNICODE);
 	}
 
 	@Override
