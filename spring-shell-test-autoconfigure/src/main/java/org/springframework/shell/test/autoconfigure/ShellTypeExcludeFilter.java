8b5288a638d82c9115d9b0f73c86c17c2e566a41
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2025-09-30 10:05:42 +0200

    Update ShellTypeExcludeFilter to latest changes in Boot 4.0

--- spring-shell-test-autoconfigure/src/main/java/org/springframework/shell/test/autoconfigure/ShellTypeExcludeFilter.java
+++ spring-shell-test-autoconfigure/src/main/java/org/springframework/shell/test/autoconfigure/ShellTypeExcludeFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2022-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ public class ShellTypeExcludeFilter extends StandardAnnotationCustomizableTypeEx
 	}
 
 	@Override
-	protected Set<Class<?>> getDefaultIncludes() {
+	protected Set<Class<?>> getKnownIncludes() {
 		return DEFAULT_INCLUDES;
 	}
 }
