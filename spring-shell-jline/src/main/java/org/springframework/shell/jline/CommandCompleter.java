1d91365aea24879c155f48211cc86b22eedf0d87
czpilar <david@czpilar.net>
2025-12-26 12:38:25 +0100

    Fix completion only for particular command option
    Resolves #1246
    
    Signed-off-by: czpilar <david@czpilar.net>

--- spring-shell-jline/src/main/java/org/springframework/shell/jline/CommandCompleter.java
+++ spring-shell-jline/src/main/java/org/springframework/shell/jline/CommandCompleter.java
@@ -12,12 +12,15 @@ import org.springframework.shell.core.command.completion.CompletionContext;
 import org.springframework.shell.core.command.completion.CompletionProposal;
 import org.springframework.shell.core.command.completion.CompletionProvider;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * A JLine {@link Completer} that completes command names from a {@link CommandRegistry}.
  *
  * @author Mahmoud Ben Hassine
+ * @author David Pilar
  * @since 4.0.0
  */
 public class CommandCompleter implements Completer {
@@ -36,20 +39,24 @@ public class CommandCompleter implements Completer {
 	public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
 		Command commandByName = findCommandByWords(line.words());
 		if (commandByName != null) {
-			// add option completions for the command
 			List<CommandOption> options = commandByName.getOptions();
-			for (CommandOption option : options) {
-				if (option.longName() != null && !line.line().contains("--" + option.longName())) {
-					candidates.add(new Candidate("--" + option.longName()));
-				}
-				if (option.shortName() != ' ' && !line.line().contains("-" + option.shortName())) {
-					candidates.add(new Candidate("-" + option.shortName()));
+			CommandOption commandOption = findOptionByWords(line.words(), options);
+			if (commandOption == null) {
+				// add option completions for the command
+				for (CommandOption option : options) {
+					boolean present = isOptionPresent(line, option);
+					if (option.longName() != null && !present) {
+						candidates.add(new Candidate("--" + option.longName()));
+					}
+					if (option.shortName() != ' ' && !present) {
+						candidates.add(new Candidate("-" + option.shortName()));
+					}
 				}
 			}
 			// add custom completions from the command's completion provider
 			CompletionProvider completionProvider = commandByName.getCompletionProvider();
-			CompletionContext context = new CompletionContext(line.words(), line.wordIndex(), line.wordCursor(), null,
-					null);
+			CompletionContext context = new CompletionContext(line.words(), line.wordIndex(), line.wordCursor(),
+					commandByName, commandOption);
 			List<CompletionProposal> proposals = completionProvider.apply(context);
 			for (CompletionProposal proposal : proposals) {
 				candidates.add(new Candidate(proposal.value()));
@@ -63,6 +70,11 @@ public class CommandCompleter implements Completer {
 		}
 	}
 
+	private boolean isOptionPresent(ParsedLine line, CommandOption option) {
+		return option.longName() != null && line.line().contains(" --" + option.longName() + " ")
+				|| option.shortName() != ' ' && line.line().contains(" -" + option.shortName() + " ");
+	}
+
 	@Nullable private Command findCommandByWords(List<String> words) {
 		StringBuilder commandName = new StringBuilder();
 		for (String word : words) {
@@ -72,6 +84,20 @@ public class CommandCompleter implements Completer {
 			commandName.append(word).append(" ");
 		}
 		return this.commandRegistry.getCommandByName(commandName.toString().trim());
+	}
+
+	@Nullable private CommandOption findOptionByWords(List<String> words, List<CommandOption> options) {
+		List<String> reversed = new ArrayList<>(words);
+		Collections.reverse(reversed);
+		String optionName = reversed.stream().filter(word -> !word.trim().isEmpty()).findFirst().orElse("");
+
+		for (CommandOption option : options) {
+			if (option.longName() != null && optionName.equals("--" + option.longName())
+					|| option.shortName() != ' ' && optionName.equals("-" + option.shortName())) {
+				return option;
+			}
+		}
+		return null;
 	}
 
 }
