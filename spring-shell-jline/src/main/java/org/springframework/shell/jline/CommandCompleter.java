29f89858dc4d23208d3e407f14eac859d9f2641a
czpilar <david@czpilar.net>
2025-12-26 13:18:55 +0100

    Fixed completion only for particular command option (#1246)
    Signed-off-by: czpilar <david@czpilar.net>

--- spring-shell-jline/src/main/java/org/springframework/shell/jline/CommandCompleter.java
+++ spring-shell-jline/src/main/java/org/springframework/shell/jline/CommandCompleter.java
@@ -39,17 +39,20 @@ public class CommandCompleter implements Completer {
 	public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
 		Command commandByName = findCommandByWords(line.words());
 		if (commandByName != null) {
-			// add option completions for the command
 			List<CommandOption> options = commandByName.getOptions();
-			for (CommandOption option : options) {
-				if (option.longName() != null && !line.line().contains("--" + option.longName())) {
-					candidates.add(new Candidate("--" + option.longName()));
-				}
-				if (option.shortName() != ' ' && !line.line().contains("-" + option.shortName())) {
-					candidates.add(new Candidate("-" + option.shortName()));
+			CommandOption commandOption = findOptionByWords(line.words(), options);
+			if (commandOption == null) {
+				// add option completions for the command
+				for (CommandOption option : options) {
+					boolean present = isOptionPresent(line, option);
+					if (option.longName() != null && !present) {
+						candidates.add(new Candidate("--" + option.longName()));
+					}
+					if (option.shortName() != ' ' && !present) {
+						candidates.add(new Candidate("-" + option.shortName()));
+					}
 				}
 			}
-			CommandOption commandOption = findOptionByWords(line.words(), options);
 			// add custom completions from the command's completion provider
 			CompletionProvider completionProvider = commandByName.getCompletionProvider();
 			CompletionContext context = new CompletionContext(line.words(), line.wordIndex(), line.wordCursor(),
@@ -65,6 +68,11 @@ public class CommandCompleter implements Completer {
 						command.getGroup(), command.getHelp(), null, null, true));
 			}
 		}
+	}
+
+	private boolean isOptionPresent(ParsedLine line, CommandOption option) {
+		return option.longName() != null && line.line().contains(" --" + option.longName() + " ")
+				|| option.shortName() != ' ' && line.line().contains(" -" + option.shortName() + " ");
 	}
 
 	@Nullable private Command findCommandByWords(List<String> words) {
