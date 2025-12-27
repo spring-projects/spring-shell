e0ef2c844cceb4c2faae0ae74faa579bcd243b34
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2025-12-28 00:17:14 +0100

    Move CommandRegistryCustomizer to core-autoconfigure module
    This API is related to Spring Boot and is better defined in the
    auto-configuration module next to other customizers.

--- spring-shell-core-autoconfigure/src/main/java/org/springframework/shell/core/autoconfigure/CommandRegistryCustomizer.java
+++ spring-shell-core-autoconfigure/src/main/java/org/springframework/shell/core/autoconfigure/CommandRegistryCustomizer.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.shell.core.command;
+package org.springframework.shell.core.autoconfigure;
+
+import org.springframework.shell.core.command.CommandRegistry;
 
 /**
  * Interface to customize a {@link CommandRegistry}.
