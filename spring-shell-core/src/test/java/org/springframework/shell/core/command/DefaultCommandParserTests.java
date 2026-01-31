spring-projects/spring-shell

commit 84abfda3dc0db9269e9cc7bfb17d17b837c62a1d

Improve argument parsing for commands without options
Before this commit, it was required to use a separator between
a command and its arguments when no options are provided.

This commit improves the default parser to be aware of the
command registry, and therefore make it able to introspect
commands and distinguish sub commands from arguments.

Resolves #1287

spring-shell-core/src/main/java/org/springframework/shell/core/command/DefaultCommandParser.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.shell.core.command;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -34,11 +35,6 @@ import org.apache.commons.logging.LogFactory;
  * Argument       ::= String
  *
  * Example: mycommand mysubcommand --optionA=value1 arg1 -b=value2 arg2 --optionC value3 -d value4
- *
- *  If subcommands are used without options, then arguments must be separated using "--" (POSIX style):
- *  CommandSyntax  ::= CommandName [SubCommandName]* '--' [Argument]*
- *
- *  Example: mycommand mysubcommand -- arg1 arg2
  * </pre>
  *
  * @author Mahmoud Ben Hassine
@@ -49,6 +45,12 @@ public class DefaultCommandParser implements CommandParser {
 
 	private static final Log log = LogFactory.getLog(DefaultCommandParser.class);
 
+	private final CommandRegistry commandRegistry;
+
+	public DefaultCommandParser(CommandRegistry commandRegistry) {
+		this.commandRegistry = commandRegistry;
+	}
+
 	@Override
 	public ParsedInput parse(String input) {
 		log.debug("Parsing input: " + input);
@@ -56,53 +58,43 @@ public class DefaultCommandParser implements CommandParser {
 
 		// the first word is the (root) command name
 		String commandName = words.get(0);
-		ParsedInput.Builder parsedInputBuilder = ParsedInput.builder().commandName(commandName);
-		if (words.size() == 1) {
-			ParsedInput parsedInput = parsedInputBuilder.build();
-			log.debug("Parsed input: " + parsedInput);
-			return parsedInput;
-		}
-
-		if (words.size() == 2 && (words.get(1).equals("--help") || words.get(1).equals("-h"))) {
-			parsedInputBuilder.addOption(CommandOption.with().shortName('h').longName("help").value("true").build());
-			ParsedInput parsedInput = parsedInputBuilder.build();
-			log.debug("Parsed input: " + parsedInput);
-			return parsedInput;
-		}
-
 		List<String> remainingWords = words.subList(1, words.size());
 
-		// parse sub commands: if no options, then need to use -- to separate sub commands
-		// from arguments (POSIX style)
-		int subCommandCount = 0;
-		for (String word : remainingWords) {
-			if (!isOption(word) && !isArgumentSeparator(word)) {
-				subCommandCount++;
-				parsedInputBuilder.addSubCommand(word);
-			}
-			else {
+		int firstNonCommandWordIndex = 1;
+		String prefix = commandName;
+		for (String remainingWord : remainingWords) {
+			String potentialCommandName = prefix + " " + remainingWord;
+			List<Command> commands = commandRegistry.getCommandsByPrefix(potentialCommandName);
+			if (commands.isEmpty()) {
 				break;
 			}
+			prefix = potentialCommandName;
+			firstNonCommandWordIndex++;
 		}
-		if (subCommandCount > 0) {
-			remainingWords = remainingWords.subList(subCommandCount, remainingWords.size());
+		List<String> subCommands = firstNonCommandWordIndex == 1 ? Collections.emptyList()
+				: words.subList(1, firstNonCommandWordIndex);
+
+		// add command and sub commands
+		ParsedInput.Builder parsedInputBuilder = ParsedInput.builder().commandName(commandName);
+		for (String subCommand : subCommands) {
+			parsedInputBuilder.addSubCommand(subCommand);
 		}
 
-		// if first remaining word is argument separator, skip it and parse remaining
-		// words as arguments
-		if (!remainingWords.isEmpty() && isArgumentSeparator(remainingWords.get(0))) {
-			remainingWords = remainingWords.subList(1, remainingWords.size());
+		// parse options and arguments
+		List<String> optionsAndArguments = words.subList(firstNonCommandWordIndex, words.size());
+		if (!optionsAndArguments.isEmpty() && isArgumentSeparator(optionsAndArguments.get(0))) {
+			optionsAndArguments = optionsAndArguments.subList(1, optionsAndArguments.size());
 			int argumentIndex = 0;
-			for (String remainingWord : remainingWords) {
+			for (String remainingWord : optionsAndArguments) {
 				CommandArgument commandArgument = parseArgument(argumentIndex++, remainingWord);
 				parsedInputBuilder.addArgument(commandArgument);
 			}
 		}
 		else { // parse remaining words as options and arguments
 			int argumentIndex = 0;
-			for (int i = 0; i < remainingWords.size(); i++) {
-				String currentWord = remainingWords.get(i);
-				String nextWord = i + 1 < remainingWords.size() ? remainingWords.get(i + 1) : null;
+			for (int i = 0; i < optionsAndArguments.size(); i++) {
+				String currentWord = optionsAndArguments.get(i);
+				String nextWord = i + 1 < optionsAndArguments.size() ? optionsAndArguments.get(i + 1) : null;
 				if (isOption(currentWord)) {
 					if (currentWord.contains("=")) {
 						CommandOption commandOption = parseOption(currentWord);

spring-shell-core/src/test/java/org/springframework/shell/core/command/DefaultCommandParserTests.java
@@ -16,6 +16,7 @@
 package org.springframework.shell.core.command;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -27,7 +28,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DefaultCommandParserTests {
 
-	private final CommandParser parser = new DefaultCommandParser();
+	private CommandParser parser;
+
+	private CommandRegistry commandRegistry;
+
+	@BeforeEach
+	void setUp() {
+		this.commandRegistry = new CommandRegistry();
+		this.parser = new DefaultCommandParser(this.commandRegistry);
+	}
 
 	@Test
 	void testParseLongOptionWithoutValue() {
@@ -80,6 +89,7 @@ class DefaultCommandParserTests {
 
 	@Test
 	void testParseCommandWithSubCommand() {
+		commandRegistry.registerCommand(createCommand("mycommand mysubcommand", "My test command"));
 		ParsedInput parsedInput = parser.parse("mycommand mysubcommand --optionA=value1 arg1 -b=value2 arg2");
 		assertEquals("mycommand", parsedInput.commandName());
 		assertEquals(1, parsedInput.subCommands().size());
@@ -108,6 +118,7 @@ class DefaultCommandParserTests {
 
 	@Test
 	void testParseWithMultipleSubCommands() {
+		commandRegistry.registerCommand(createCommand("mycommand mysubcommand1 mysubcommand2", "My test command"));
 		ParsedInput parsedInput = parser
 			.parse("mycommand mysubcommand1 mysubcommand2 --optionA=value1 arg1 -b=value2 arg2");
 		assertEquals("mycommand", parsedInput.commandName());
@@ -138,6 +149,7 @@ class DefaultCommandParserTests {
 
 	@Test
 	void testParseWithSubCommandWithoutOptions() {
+		commandRegistry.registerCommand(createCommand("mycommand mysubcommand", "My test command"));
 		ParsedInput parsedInput = parser.parse("mycommand mysubcommand -- arg1 arg2");
 		assertEquals("mycommand", parsedInput.commandName());
 		assertEquals(1, parsedInput.subCommands().size());
@@ -154,15 +166,17 @@ class DefaultCommandParserTests {
 		assertEquals("arg2", argument2.value());
 	}
 
-	// In this case, arguments are considered as subcommands since there's no separator
+	// In this case, subcommands should still be parsed, and the rest considered as
+	// arguments
 	@Test
 	void testParseWithSubCommandWithoutOptionsAndWithoutSeparator() {
+		commandRegistry.registerCommand(createCommand("mycommand mysubcommand", "My test command"));
 		ParsedInput parsedInput = parser.parse("mycommand mysubcommand arg1 arg2");
 		assertEquals("mycommand", parsedInput.commandName());
-		assertEquals(3, parsedInput.subCommands().size());
+		assertEquals(1, parsedInput.subCommands().size());
 		assertEquals("mysubcommand", parsedInput.subCommands().get(0));
-		assertEquals("arg1", parsedInput.subCommands().get(1));
-		assertEquals("arg2", parsedInput.subCommands().get(2));
+		assertEquals("arg1", parsedInput.arguments().get(0).value());
+		assertEquals("arg2", parsedInput.arguments().get(1).value());
 	}
 
 	@ParameterizedTest
@@ -253,6 +267,15 @@ class DefaultCommandParserTests {
 				Arguments.of("mycommand -- value1'inside'value2", "value1'inside'value2"),
 				Arguments.of("mycommand -- \"value1 'inside' value2\"", "value1 'inside' value2"),
 				Arguments.of("mycommand  --  value", "value"), Arguments.of("mycommand  --  \"value\"", "value"));
+	}
+
+	private static Command createCommand(String name, String description) {
+		return new AbstractCommand(name, description) {
+			@Override
+			public ExitStatus doExecute(CommandContext commandContext) {
+				return ExitStatus.OK;
+			}
+		};
 	}
 
 }

spring-shell-core-autoconfigure/src/main/java/org/springframework/shell/core/autoconfigure/ShellRunnerAutoConfiguration.java
@@ -66,8 +66,8 @@ public class ShellRunnerAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	public CommandParser commandParser() {
-		return new DefaultCommandParser();
+	public CommandParser commandParser(CommandRegistry commandRegistry) {
+		return new DefaultCommandParser(commandRegistry);
 	}
 
 }

spring-shell-core-autoconfigure/src/main/java/org/springframework/shell/core/autoconfigure/StandardCommandsAutoConfiguration.java
@@ -73,8 +73,8 @@ public class StandardCommandsAutoConfiguration {
 
 	@Bean
 	@ConditionalOnProperty(value = "spring.shell.command.script.enabled", havingValue = "true", matchIfMissing = true)
-	public Command scriptCommand() {
-		return new Script();
+	public Command scriptCommand(CommandRegistry commandRegistry) {
+		return new Script(commandRegistry);
 	}
 
 }

spring-shell-core/src/main/java/org/springframework/shell/core/command/Script.java
@@ -32,7 +32,11 @@ import org.springframework.shell.core.NonInteractiveShellRunner;
  */
 public class Script implements Command {
 
-	private CommandParser commandParser = new DefaultCommandParser();
+	private CommandParser commandParser;
+
+	public Script(CommandRegistry commandRegistry) {
+		this.commandParser = new DefaultCommandParser(commandRegistry);
+	}
 
 	@Override
 	public String getDescription() {

spring-shell-core/src/main/java/org/springframework/shell/core/command/annotation/support/EnableCommandRegistrar.java
@@ -63,13 +63,21 @@ public final class EnableCommandRegistrar implements ImportBeanDefinitionRegistr
 			registry.registerBeanDefinition("commandRegistry", beanDefinition);
 		}
 
+		// register command parser
+		if (!registry.containsBeanDefinition("commandParser")) {
+			BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
+				.genericBeanDefinition(DefaultCommandParser.class)
+				.addConstructorArgReference("commandRegistry");
+			registry.registerBeanDefinition("commandParser", beanDefinitionBuilder.getBeanDefinition());
+		}
+
 		// register shell runner (default to interactive if none is defined)
 		if (!registry.containsBeanDefinition("shellRunner")) {
 			boolean debugModeValue = System.getProperty("spring.shell.debug.enabled", "false").equalsIgnoreCase("true");
 			BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
 				.genericBeanDefinition(SystemShellRunner.class)
 				.addConstructorArgValue(new ConsoleInputProvider())
-				.addConstructorArgValue(new DefaultCommandParser())
+				.addConstructorArgReference("commandParser")
 				.addConstructorArgReference("commandRegistry")
 				.addPropertyValue("debugMode", debugModeValue);
 			registry.registerBeanDefinition("shellRunner", beanDefinitionBuilder.getBeanDefinition());

spring-shell-docs/src/test/java/org/springframework/shell/docs/CommandAnnotationSnippets.java
@@ -51,7 +51,7 @@ class CommandAnnotationSnippets {
 					@Argument(index = 0, description = "the name of the person to greet",
 							defaultValue = "world") String name,
 					@Option(shortName = 's', longName = "suffix", description = "the suffix of the greeting message",
-							defaultValue = "!") char suffix) {
+							defaultValue = "!") String suffix) {
 				System.out.println("Hi " + name + suffix);
 			}
 

spring-shell-jline/src/main/java/org/springframework/shell/jline/DefaultJLineShellConfiguration.java
@@ -20,7 +20,7 @@ public class DefaultJLineShellConfiguration {
 
 	@Bean
 	public ShellRunner shellRunner(JLineInputProvider inputProvider, CommandRegistry commandRegistry) {
-		return new JLineShellRunner(inputProvider, new DefaultCommandParser(), commandRegistry);
+		return new JLineShellRunner(inputProvider, new DefaultCommandParser(commandRegistry), commandRegistry);
 	}
 
 	@Bean

commit fe806a011712ad70d08ad700569049f12e795c9a

Fix regression in command help options
The regression was introduced in 84abfda3

Signed-off-by: David Pilar <david@czpilar.net>

spring-shell-core/src/main/java/org/springframework/shell/core/command/DefaultCommandParser.java
@@ -59,6 +59,20 @@ public class DefaultCommandParser implements CommandParser {
 
 		// the first word is the (root) command name
 		String commandName = words.get(0);
+		ParsedInput.Builder parsedInputBuilder = ParsedInput.builder().commandName(commandName);
+		if (words.size() == 1) {
+			ParsedInput parsedInput = parsedInputBuilder.build();
+			log.debug("Parsed input: " + parsedInput);
+			return parsedInput;
+		}
+
+		if (words.size() == 2 && (words.get(1).equals("--help") || words.get(1).equals("-h"))) {
+			parsedInputBuilder.addOption(CommandOption.with().shortName('h').longName("help").value("true").build());
+			ParsedInput parsedInput = parsedInputBuilder.build();
+			log.debug("Parsed input: " + parsedInput);
+			return parsedInput;
+		}
+
 		List<String> remainingWords = words.subList(1, words.size());
 
 		int firstNonCommandWordIndex = 1;
@@ -76,7 +90,6 @@ public class DefaultCommandParser implements CommandParser {
 				: words.subList(1, firstNonCommandWordIndex);
 
 		// add command and sub commands
-		ParsedInput.Builder parsedInputBuilder = ParsedInput.builder().commandName(commandName);
 		for (String subCommand : subCommands) {
 			parsedInputBuilder.addSubCommand(subCommand);
 		}

spring-shell-core/src/test/java/org/springframework/shell/core/command/DefaultCommandParserTests.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -312,6 +313,24 @@ class DefaultCommandParserTests {
 				Arguments.of("mycommand -o true", "", 'o', Boolean.class, "true"),
 				Arguments.of("mycommand -o false", "", 'o', Boolean.class, "false"),
 				Arguments.of("mycommand -o", "", 'o', Boolean.class, "true"));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "mycommand --help", "mycommand -h" })
+	void testParseWithHelpOption(String input) {
+		// given
+		Command command = createCommand("mycommand", "My test command");
+		command.getOptions().add(CommandOption.with().longName("option").shortName('o').build());
+		commandRegistry.registerCommand(command);
+		// when
+		ParsedInput parsedInput = parser.parse(input);
+
+		// then
+		assertEquals("mycommand", parsedInput.commandName());
+		assertEquals(1, parsedInput.options().size());
+		assertEquals("help", parsedInput.options().get(0).longName());
+		assertEquals('h', parsedInput.options().get(0).shortName());
+		assertEquals("true", parsedInput.options().get(0).value());
 	}
 
 	private static Command createCommand(String name, String description) {