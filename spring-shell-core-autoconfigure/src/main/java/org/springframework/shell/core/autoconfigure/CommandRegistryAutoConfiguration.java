236dbc99ad6d1cedfe56fef2d913cb13670df302
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2026-03-30 18:43:02 +0200

    Fix annotated commands registration in GraalVM native image
    Before this commit, annotated commands where fetched by scanning the
    classpath for component types at runtime, which native image can't do.
    
    This commit fixes that by looking for candidate components as beans
    from the application context.
    
    Resolves #1229

--- spring-shell-core-autoconfigure/src/main/java/org/springframework/shell/core/autoconfigure/CommandRegistryAutoConfiguration.java
+++ spring-shell-core-autoconfigure/src/main/java/org/springframework/shell/core/autoconfigure/CommandRegistryAutoConfiguration.java
@@ -22,22 +22,17 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.springframework.aop.support.AopUtils;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.MethodIntrospector;
 import org.springframework.core.annotation.AnnotatedElementUtils;
-import org.springframework.shell.core.ShellConfigurationException;
 import org.springframework.shell.core.command.Command;
 import org.springframework.shell.core.command.CommandRegistry;
 import org.springframework.shell.core.command.annotation.support.CommandFactoryBean;
 import org.springframework.shell.core.utils.Utils;
-import org.springframework.util.ClassUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
 @AutoConfiguration
@@ -61,33 +56,21 @@ public class CommandRegistryAutoConfiguration {
 	}
 
 	private void registerAnnotatedCommands(ApplicationContext applicationContext, CommandRegistry commandRegistry) {
-		Map<String, Object> springBootApps = applicationContext.getBeansWithAnnotation(SpringBootApplication.class);
-		Class<?> mainClass = AopUtils.getTargetClass(springBootApps.values().iterator().next());
-		String mainPackage = ClassUtils.getPackageName(mainClass);
-		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(true);
-		Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents(mainPackage);
-		for (BeanDefinition candidateComponent : candidateComponents) {
-			String className = candidateComponent.getBeanClassName();
-			if (className == null) {
-				log.warn(String.format("Skipping candidate component %s with null class name", candidateComponent));
+		Map<String, Object> components = applicationContext.getBeansWithAnnotation(Component.class);
+		for (Object candidateComponent : components.values()) {
+			Class<?> type = candidateComponent.getClass();
+			String className = type.getName();
+			if (className.startsWith("org.springframework.boot")) {
 				continue;
 			}
-			else {
-				log.debug("Registering commands from component: " + className);
-			}
-			try {
-				Class<?> cls = ClassUtils.forName(className, applicationContext.getClassLoader());
-				ReflectionUtils.MethodFilter filter = method -> AnnotatedElementUtils.hasAnnotation(method,
-						org.springframework.shell.core.command.annotation.Command.class);
-				Set<Method> methods = MethodIntrospector.selectMethods(cls, filter);
-				for (Method method : methods) {
-					CommandFactoryBean factoryBean = new CommandFactoryBean(method);
-					factoryBean.setApplicationContext(applicationContext);
-					commandRegistry.registerCommand(factoryBean.getObject());
-				}
-			}
-			catch (ClassNotFoundException e) {
-				throw new ShellConfigurationException("Unable to configure commands from class " + className, e);
+			log.debug("Registering commands from component: " + className);
+			ReflectionUtils.MethodFilter filter = method -> AnnotatedElementUtils.hasAnnotation(method,
+					org.springframework.shell.core.command.annotation.Command.class);
+			Set<Method> methods = MethodIntrospector.selectMethods(type, filter);
+			for (Method method : methods) {
+				CommandFactoryBean factoryBean = new CommandFactoryBean(method);
+				factoryBean.setApplicationContext(applicationContext);
+				commandRegistry.registerCommand(factoryBean.getObject());
 			}
 		}
 	}
